9393a49020415bca1c33aaa877fbf82e8e4fc1ab
04banshee <54637063+04banshee@users.noreply.github.com>
2019-10-08 09:52:15 -0500

    Add files via upload

---
+++ scripts/myScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  var person = prompt("Please enter your name");
+  if (person != null) {
+    alert("Hello " + person + "! How are you today?");
+	console.log(person);
+  }
+}
